fa71b97adcb56075d0140fe39e495658feddf0e4
Pahrul Irfan <ip.5090@gmail.com>
2022-01-07 10:08:27 +0800

    logout

--- crud/index.php
+++ crud/index.php
@@ -1,59 +1,86 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+    header('location: login.php');
+}
+
+?>
+
 <!doctype html>
 <html>
+
 <head>
     <title>Data Artikel</title>
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
 </head>
+
 <body>
-<div class="container">
-    <h2 class="alert alert-info">Data Artikel</h2>
+    <div class="container">
+        <h2 class="alert alert-info">
 
-    <a href="tambah.php" class="btn btn-info">Tambah Data</a>
+            Data Artikel, Selamat Datang :
+            <?php echo $_SESSION['fullname']; ?>
 
-    <table class="table table-bordered">
-        <thead>
-        <tr>
-            <th>No</th>
-            <th>Author</th>
-            <th>Title</th>
-            <th>Body</th>
-            <th>Keyword</th>
-            <th>Aksi</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        require 'setting.php';
-        $query = "SELECT * FROM article";
-        $sql = mysqli_query($koneksi, $query);
-        $no = 1;
-        while ($data = mysqli_fetch_object($sql)) {
-        ?>
+        </h2>
 
-        <tr>
-            <td> <?php echo $no++; ?> </td>
-            <td> <?php echo $data->author; ?> </td>
-            <td> <?php echo $data->title; ?> </td>
-            <td> <?php echo $data->body; ?> </td>
-            <td> <?php echo $data->keyword; ?> </td>
-            <td>
-                <a href="edit.php?id=<?= $data->id;?>">
-                    <input type="submit" value="edit" class="btn btn-warning">
-                </a>
-                <a href="hapus.php?id=<?= $data->id?>">
-                    <input type="submit" value="hapus" onclick="confirm('yakin hapus data?')" class="btn btn-danger">
-                </a>
-            </td>
+        <a href="tambah.php" class="btn btn-info">Tambah Data</a>
 
-        </tr>
+        <a href="logout.php" class="btn btn-secondary float-end mb-3">Logout</a>
 
-        <?php
-        }
-        ?>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Author</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                    <th>Keyword</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                require 'setting.php';
+                $query = "SELECT * FROM article";
+                $sql = mysqli_query($koneksi, $query);
+                $no = 1;
+                while ($data = mysqli_fetch_object($sql)) {
+                ?>
+
+                    <tr>
+                        <td> <?php echo $no++; ?> </td>
+                        <td> <?php echo $data->author; ?> </td>
+                        <td> <?php echo $data->title; ?> </td>
+                        <td> <?php echo $data->body; ?> </td>
+                        <td> <?php echo $data->keyword; ?> </td>
+                        <td>
+                            <a href="edit.php?id=<?= $data->id; ?>">
+                                <input type="submit" value="edit" class="btn btn-warning">
+                            </a>
+
+                            <?php
+                            if ($_SESSION['role'] == 'admin') {
+                            ?>
+                                <a href="hapus.php?id=<?= $data->id ?>">
+                                    <input type="submit" value="hapus" onclick="confirm('yakin hapus data?')" class="btn btn-danger">
+                                </a>
+                            <?php
+                            }
+                            ?>
+
+                        </td>
+
+                    </tr>
+
+                <?php
+                }
+                ?>
 
 
-        </tbody>
-    </table>
-</div>
+            </tbody>
+        </table>
+    </div>
 </body>
+
 </html>
